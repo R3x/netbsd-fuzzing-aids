842318b7ba3bb3e3d4b32e25bc87b7a21e7822b3
R3x <siddharth.muralee@gmail.com>
2019-06-28 08:11:48 +0000

    finalize modify_struct.py

--- modify_struct.py
+++ modify_struct.py
@@ -3,14 +3,14 @@ import sys
 import os
 
 def usage():
-    print "USAGE: python modify_header.py <headerfile>"
+    print "USAGE: python modify_header.py <headerfile> <struct name>"
 
 typedef_stored = {}
 
 def main():
     struct_finder = re.compile("typedef struct \{([^}]*?)\} (.*);")
-    typedef_finder = re.compile("typedef (.*) (.*?)[\[;]")
-    member_finder = re.compile("[^t\n]*? (.*) (.*);")
+    typedef_finder = re.compile("typedef[ \t](.*)[ \t](.*?)[\[;]")
+    member_finder = re.compile("[^t\n]*?[ \t](.*)[ \t](.*);")
     if len(sys.argv) < 2:
         usage()
         exit()
@@ -22,12 +22,14 @@ def main():
     structs = struct_finder.findall(content)
     typedefs = typedef_finder.findall(content)
     for typedef in typedefs:
-        typedef_stored[typedef[1].strip()] = typedef[0].strip()
+        typedef_stored[typedef[1].strip().strip('\t')] = typedef[0].strip().strip('\t')
     print typedef_stored
     wfp.write(content)
     for struct in structs:
         print "structs found " + struct[1]
-        wfp.write("\ntypedef struct  _" + struct[1] + "\t" + struct[1]) 
+        if cmp(struct[1], sys.argv[2]) != 0:
+            continue
+        wfp.write("\ntypedef struct  " + struct[1] + "\t_" + struct[1] + ";") 
         wfp.write("\nstruct " + struct[1] + "{" + struct[0] + "};")
     wfp.seek(0)
     content = wfp.readlines()
@@ -36,21 +38,22 @@ def main():
     for line in content:
         match = member_finder.match(line)
         if match:
-            if typedef_stored.has_key(match.groups()[0].strip()):
-                line = line.replace(match.groups()[0].strip(), typedef_stored[match.groups()[0].strip()])
+            if typedef_stored.has_key(match.groups()[0].strip().strip('\t')):
+                line = line.replace(match.groups()[0].strip().strip('\t'), typedef_stored[match.groups()[0].strip().strip('\t')])
                 print line
         content2.append(line)
     for line in content2:
         match = member_finder.match(line)
         if match:
-            if typedef_stored.has_key(match.groups()[0].strip()):
-                line = line.replace(match.groups()[0].strip(), typedef_stored[match.groups()[0].strip()])
+            if typedef_stored.has_key(match.groups()[0].strip().strip('\t')):
+                line = line.replace(match.groups()[0].strip().strip('\t'), typedef_stored[match.groups()[0].strip().strip('\t')])
                 print line
         content3.append(line)
     rfp.writelines(content3)
     wfp.close()
     rfp.close()
     #os.rename("temp.h", sys.argv[1])
+    os.rename("temp2.h", sys.argv[1])
 
 if __name__ == "__main__":
     main()
